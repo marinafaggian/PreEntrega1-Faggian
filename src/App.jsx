f086a4322605a9b1667e664e7cc5bc6a1970ab1b
marinafaggian <marinafaggian92@gmail.com>
2023-10-06 12:13:15 -0300

    limpio archivos

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 const App = () => {
   return (
     <NavBar/>
-    <ItemListContainer greeting="¡Bienvenidos!"/>
+    // <ItemListContainer greeting="¡Bienvenidos!"/>
     )
 }
 
