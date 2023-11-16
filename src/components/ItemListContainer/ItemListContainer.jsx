fda464d1dbcd463467f4d0e0fbf4cbdd4fb86760
marinafaggian <marinafaggian92@gmail.com>
2023-11-16 12:45:56 -0300

    incorporo firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,26 +1,32 @@
 import { useState, useEffect } from "react";
-import { getProductos, getProductorPorCategoria } from "../../asyncmock";
 import ItemList from '../ItemList/ItemList';
 import { useParams } from "react-router-dom";
+import { db } from "../../service/config";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const ItemListContainer = () => {
   const [productos, setProductos] = useState([]);
 
-  const {idCategoria} = useParams();
+  const { idCategoria } = useParams();
 
   useEffect(() => {
+    const misProductos = idCategoria ? query(collection(db, "productos"), where("idCat", "==", idCategoria)) : collection(db, "productos");
 
-    const funcionProductos = idCategoria ? getProductorPorCategoria : getProductos;
-    
-    funcionProductos(idCategoria)
-      .then(res => setProductos(res))
-    
-  }, [idCategoria])
-  
-    return (
+    getDocs(misProductos)
+      .then(res => {
+        const nuevosProductos = res.docs.map(doc => {
+          const data  = doc.data()
+          return {id:doc.id, ...data}
+        })
+        setProductos(nuevosProductos);
+      })
+      .catch(error => console.log(error));
+    }, [idCategoria]);
+
+  return (
     <>
-        <h2 style={{textAlign:'center'}}>Mis Productos</h2>
-        <ItemList productos = {productos}/>
+      <h2 style={{ textAlign: 'center' }}>Mis Productos</h2>
+      <ItemList productos={productos} />
     </>
   )
 }
